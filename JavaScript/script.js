ad2d196187dc937eebe23973b154c8558a1f4fbe
compguy123 <77632039+compguy123@users.noreply.github.com>
2022-11-16 17:24:05 -0600

    js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,10 +3,38 @@ alert("Hello World");
 }
 
 
-function cs(){
-    alert("Cs");
+async function getGames(){
+    const jsonPath ="../JavaScript/data.json";
+    const response = await fetch(jsonPath);
+    const games = await response.json();
+    return games;    
 }
 
-function dota(){
-    alert("Dota");
+function getGamesContainer(){
+    return document.querySelector(".gamesContainer");
 }
+
+document.addEventListener("DOMContentLoaded",async x =>{
+    const gamesContainer = getGamesContainer();
+    const games = await getGames();
+    for(const game of games){
+        const cardHTML =
+         `<div class="card" style="width: 18rem">
+        <img
+          class="card-img-top"
+          src="${game.url}"
+          alt="Card image cap"
+        />
+        <div class="card-body">
+          <h5 class="card-title">${game.gameTitle}</h5>
+          <p class="card-text">
+            ${game.description}
+          </p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>`
+      gamesContainer.insertAdjacentHTML("afterbegin",cardHTML);
+    }
+    
+})
+
