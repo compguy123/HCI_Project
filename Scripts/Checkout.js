45b7ccfe8354124e79aa2d3244c2337307d0e15d
dipeshasd <dipesh.sah@hotmail.com>
2022-11-27 14:55:15 -0600

    fix breadcrumb checkout

--- Scripts/Checkout.js
+++ Scripts/Checkout.js
@@ -2,3 +2,12 @@ window.onload = function () {
   let cart = localStorage.getItem('cart');
   document.querySelector(".odrTotal").innerHTML=JSON.parse(cart).total;
 }
+
+function handlePurchase(e) {
+  e.preventDefault();
+  localStorage.removeItem("cart");
+  //redirect to Index.html
+  window.location.href = "../Index.html";
+  
+  alert("THANK YOU FOR YOUR PURCHASE")
+}
